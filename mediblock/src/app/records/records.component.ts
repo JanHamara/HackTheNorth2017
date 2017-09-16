3762bd70c81c77293e1958f6d4a4843b6d5d271f
Jan Hamara <jancimac@v1021-wn-91-94.campus-dynamic.uwaterloo.ca>
2017-09-16 17:39:41 -0400

    started list of records with mockdata

--- mediblock/src/app/records/records.component.ts
+++ mediblock/src/app/records/records.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {consoleTestResultsHandler} from "tslint/lib/test";
 
 @Component({
   selector: 'app-records',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./records.component.less']
 })
 export class RecordsComponent implements OnInit {
-
+  records: Object[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.records = [
+      {id: 1, cat: 'control', name: 'Annual eyesight control', date: '18/08/2017',
+        time: '14:22', place: 'St.Mary Clinic, London'},
+      {id: 2, cat: 'illness', name: 'Hay Fever', date: '24/05/2017',
+        time: '09:56', place: 'GP / High Street, London'},
+      {id: 3, cat: 'vaccine', name: 'MIT Vaccination', date: '09/04/2017',
+        time: '12:00', place: 'GP / High Street, London'}
+    ];
+    console.log(this.records);
   }
 
 }
