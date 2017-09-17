7930cad57fe6aeb2715ad63e88a27b206774a036
Jan Hamara <jancimac@v1020-wn-218-74.campus-dynamic.uwaterloo.ca>
2017-09-17 00:17:31 -0400

    finished search engine

--- mediblock/src/app/dashboard/dashboard.component.ts
+++ mediblock/src/app/dashboard/dashboard.component.ts
@@ -6,9 +6,10 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less']
 })
-export class DashboardComponent implements OnInit, OnChanges {
+export class DashboardComponent implements OnInit {
   searchterm: string;
   searchstate = 'empty';
+  patients: String[] = [];
 
   recordsObservable: FirebaseListObservable<any[]>;
   records: any;
@@ -20,15 +21,24 @@ export class DashboardComponent implements OnInit, OnChanges {
   ngOnInit() {
     this.recordsObservable.subscribe((response) => {
       this.records = response;
-      console.log('Data: ', this.records);
+      this.records.forEach((record) => {
+        this.patients.push(record.patientData.firstName);
+        this.patients.push(record.patientData.secondName);
+      });
     });
   }
-
-  ngOnChanges() {
+  search() {
     if (this.searchterm == null) {
       this.searchstate = 'empty';
+      console.log('Input is empty');
     } else {
-
+      this.searchstate = 'searching';
+      console.log('searching');
+      this.patients.forEach((patient) => {
+        if (patient === this.searchterm) {
+          this.searchstate = 'found';
+        }
+      })
     }
   }
 
